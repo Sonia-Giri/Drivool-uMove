a467f5ee343a06e48be900eacbdd6cdcfe8e503e
Sonia <147712526+Sonia-Giri@users.noreply.github.com>
2023-12-23 19:24:16 +0530

    Create clickHandler.js

---
+++ js/clickHandler.js
@@ -0,0 +1,21 @@
+//menu items
+function onClickChangeAcc() {}
+function onClickManageAcc() {}
+function onClickManageDev() {}
+function onClickPortal() {}
+function onClickSimRecharge() {}
+function onClickGpsRecharge() {}
+function onClickDrivoolShop() {}
+function onClickHelp() {}
+function onClickLiveChat() {}
+function onClickAbout() {}
+
+//tile icons
+function onClickTrack() {}
+function onClickVehicles() {}
+function onClickQuickShare() {}
+function onClickHistory() {}
+function onClickAnalytics() {}
+function onClickAlert() {}
+function onClickManageUsers() {}
+function onClickDrivoolShop() {}
