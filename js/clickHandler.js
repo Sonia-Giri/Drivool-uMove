aaa7dfd3829b21286b2a8ed9319cc695133d2f12
Sonia <147712526+Sonia-Giri@users.noreply.github.com>
2024-01-09 16:44:23 +0530

    setTimeout and function for vehicles tab

--- js/clickHandler.js
+++ js/clickHandler.js
@@ -17,8 +17,31 @@ function onClickVehicles() {setTimeout(function () {
     titlebar4vehicles();
   }, 600);
 
-  function titlebar4vehicles() {
-    console.log("hello");
+ function titlebar4vehicles() {
+    $("#leftside-header div").html("Vehicles()").css("font-size", "22px");
+
+    var vehiclesRHeader = `<img
+    src="img/search.png"
+    id="search-icon"
+    height="36px"
+    width="auto"    
+  />
+  <img
+    src="img/add_gps.png"
+    id="addGPS-icon"
+    height="36px"
+    width="auto"
+    style="margin-left: 20px"
+  />
+  <img
+    src="img/add_phone.png"
+    id="addPHN-icon"
+    height="36px"
+    width="auto"
+    style="margin-left: 20px"
+  />`;
+
+    $("#rightside-header").html(vehiclesRHeader);
   }
 }
 function onClickQuickShare() {}
