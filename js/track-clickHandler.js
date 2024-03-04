9dd4b78573894b8e2037160d8cd253fc0e9640ea
Sonia <147712526+Sonia-Giri@users.noreply.github.com>
2024-03-04 09:14:43 +0530

    Create track-clickHandler.js

---
+++ js/track-clickHandler.js
@@ -0,0 +1,36 @@
+//MENU ITEMS (3 DOTS MENU)
+function onClickManagePOIs() {
+  console.log("You've clicked on Manage POIs");
+}
+// function onClickPreferences() {
+//   console.log("You've clicked on Preferences");
+// }
+function onClickShowAllVehicles() {
+  console.log("You've clicked on Show All Vehicles");
+}
+function onClickDailyTripReport() {
+  console.log("You've clicked on Daily Trip Report");
+}
+
+//LABELS
+function onClickTrackDate() {
+  console.log("You've clicked on Date");
+}
+function onClickTrackSpeed() {
+  console.log("You've clicked on Speed");
+}
+function onClickTrackVolt() {
+  console.log("You've clicked on Volt");
+}
+function onClickTrackSignal() {
+  console.log("You've clicked on Signal");
+}
+function onClickTrackLocation() {
+  console.log("You've clicked on Location");
+}
+function onClickTrackMeter() {
+  console.log("You've clicked on Meter");
+}
+function onClickTrackDistance() {
+  console.log("You've clicked on Distance");
+}
