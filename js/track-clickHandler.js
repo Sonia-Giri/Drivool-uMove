c50df227a735585d75b97e7bdb325c9b4234c15b
Sonia <147712526+Sonia-Giri@users.noreply.github.com>
2024-03-07 12:43:13 +0530

    update onClickPreferences and functions for labels

--- js/track-clickHandler.js
+++ js/track-clickHandler.js
@@ -2,9 +2,26 @@
 function onClickManagePOIs() {
   console.log("You've clicked on Manage POIs");
 }
-// function onClickPreferences() {
-//   console.log("You've clicked on Preferences");
-// }
+
+function onClickPreferences() {
+  console.log("You've clicked on Preferences");
+  $("#preferencesDialog").load("map_settings.html");
+  $(".transparentdiv").css("height", "100%");
+  $("#threeDotsDiv").css("right", "-230px");
+
+  var trackDialogDiv = $("#trackDialogDiv");
+  if (
+    trackDialogDiv.css("display") === "none" ||
+    trackDialogDiv.css("display") === ""
+  ) {
+    trackDialogDiv.css("display", "block");
+    trackDialogDiv.css("animation", "growDialog 0.3s ease-in-out forwards");
+  } else {
+    console.log("hlo");
+    closeTrackDialogDiv();
+  }
+}
+
 function onClickShowAllVehicles() {
   console.log("You've clicked on Show All Vehicles");
 }
@@ -18,18 +35,26 @@ function onClickTrackDate() {
 }
 function onClickTrackSpeed() {
   console.log("You've clicked on Speed");
+  showBox();
+  $(".messageDialog").html("Speed is 0km/h");
 }
 function onClickTrackVolt() {
   console.log("You've clicked on Volt");
+  showBox();
+  $(".messageDialog").html("Battery Volatge is 12.24V");
 }
 function onClickTrackSignal() {
   console.log("You've clicked on Signal");
+  showBox();
+  $(".messageDialog").html("Network Signal Strength is 9 out of 10");
 }
 function onClickTrackLocation() {
   console.log("You've clicked on Location");
 }
-function onClickTrackMeter() {
-  console.log("You've clicked on Meter");
+function onClickTrackAltitude() {
+  console.log("You've clicked on Altitude");
+  showBox();
+  $(".messageDialog").html("Altitude from mean sea level is 1m");
 }
 function onClickTrackDistance() {
   console.log("You've clicked on Distance");
