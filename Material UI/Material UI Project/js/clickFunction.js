a342e038ebb15043c46a4e87958508f2659f8317
Sonia <147712526+Sonia-Giri@users.noreply.github.com>
2024-04-02 16:40:28 +0530

    Create clickFunction.js

---
+++ Material UI/Material UI Project/js/clickFunction.js
@@ -0,0 +1,43 @@
+function onClickStartupMsg() {
+  console.log("You have clicked on Startup Msg.");
+}
+
+function onClickFlashMsg() {
+  console.log("You have clicked on Flash Msg.");
+}
+function onClickNotification() {
+  console.log("You have clicked on Notification.");
+}
+function onClickAssignSim() {
+  console.log("You have clicked on Assign Sim.");
+}
+function onClickSimRecharge() {
+  console.log("You have clicked on SIM Recharge.");
+}
+function onClickResetApiKey() {
+  console.log("You have clicked on Reset API Key.");
+}
+function onClickMap() {
+  console.log("You have clicked on Map.");
+}
+function onClickHistory() {
+  console.log("You have clicked on History.");
+}
+function onClickGpsRecharge() {
+  console.log("You have clicked on GPS Recharge.");
+}
+function onClickSell() {
+  console.log("You have clicked on Sell.");
+}
+function onClickTab1() {
+  console.log("You have clicked on Tab 1.");
+}
+function onClickTab2() {
+  console.log("You have clicked on Tab 2.");
+}
+function onClickTab3() {
+  console.log("You have clicked on Tab 3.");
+}
+function onClickTab4() {
+  console.log("You have clicked on Tab 4.");
+}
